6eac27fb989512f8683efa9d1ac0f744e4cf8b5c
bilal8423 <bkhan8423@gmail.com>
2023-01-28 17:21:30 +0500

    first commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+// const express = require('express');
+
+// const app = express();
+import express from "express";
+import router from "./router";
+import morgan from "morgan";
+import cors from "cors";
+import { protect } from "./modules/auth";
+
+const app = express();
+
+app.use(cors()); //By default everyone can access the server
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.json("Hey there mate!");
+});
+
+app.use("/api", protect, router);
+
+export default app;
